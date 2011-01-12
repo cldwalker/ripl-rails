0413ce682eb11676bfa08438217572796cc250c1
Gabriel Horner <gabriel.horner@gmail.com>
2011-01-12 11:32:25 -0500

    update plugin layout

--- lib/ripl/rails.rb
+++ lib/ripl/rails.rb
@@ -1,30 +1,30 @@
-module Ripl
-  module Rails
-    VERSION = '0.1.0'
+require 'ripl'
 
-    def before_loop
-      load_rails
-      super
+module Ripl::Rails
+  VERSION = '0.1.0'
+
+  def before_loop
+    load_rails
+    super
+  end
+
+  def load_rails
+    abort "Not in a Rails environment" unless File.exists?("#{Dir.pwd}/config/boot.rb")
+    ENV['RAILS_ENV'] = ARGV[0] if ARGV[0].to_s[/^[^-]/]
+
+    require "#{Dir.pwd}/config/boot"
+    require 'rails' unless defined? ::Rails
+    if ::Rails.version >= '3.0'
+      Object.const_set :APP_PATH, File.expand_path("#{Dir.pwd}/config/application")
+      require 'rails/console/app'
+      require 'rails/console/helpers'
+      require APP_PATH
+      ::Rails.application.require_environment!
+    else
+      ["#{Dir.pwd}/config/environment", 'console_app', 'console_with_helpers'].each {|e| require e }
     end
-
-    def load_rails
-      abort "Not in a Rails environment" unless File.exists?("#{Dir.pwd}/config/boot.rb")
-      ENV['RAILS_ENV'] = ARGV[0] if ARGV[0].to_s[/^[^-]/]
-
-      require "#{Dir.pwd}/config/boot"
-      require 'rails' unless defined? ::Rails
-      if ::Rails.version >= '3.0'
-        Object.const_set :APP_PATH, File.expand_path("#{Dir.pwd}/config/application")
-        require 'rails/console/app'
-        require 'rails/console/helpers'
-        require APP_PATH
-        ::Rails.application.require_environment!
-      else
-        ["#{Dir.pwd}/config/environment", 'console_app', 'console_with_helpers'].each {|e| require e }
-      end
-      puts "Loading #{::Rails.env} environment (Rails #{::Rails.version})"
-    end
+    puts "Loading #{::Rails.env} environment (Rails #{::Rails.version})"
   end
 end
 
-Ripl::Shell.send :include, Ripl::Rails if defined? Ripl::Shell
+Ripl::Shell.include Ripl::Rails
