f37c4994b3b5a8b2953d4c76ac13df4f1d7edf35
Jan Lelis <mail@janlelis.de>
2011-03-29 18:03:21 +0200

    allow usage from a rails app's subdirectory

--- lib/ripl/rails.rb
+++ lib/ripl/rails.rb
@@ -1,14 +1,40 @@
+require 'ripl'
+require 'pathname'
+
 module Ripl
   module Rails
     VERSION = '0.1.1'
 
+    class << self # inspired by the Rails 3 source
+      def find_rails_root!
+        cwd = Dir.pwd
+        return false unless in_rails_application? || in_rails_application_subdirectory?
+        return true if in_rails_application?
+        Dir.chdir '..'
+        find_rails_root! unless cwd == Dir.pwd
+      rescue SystemCallError
+        return false # could not chdir
+      end
+
+      def in_rails_application?
+        File.exists?(File.join [Dir.pwd, 'config', 'boot.rb'])
+      end
+
+      def in_rails_application_subdirectory?(path = Pathname.new(Dir.pwd))
+        File.exists?(File.join [path, 'config', 'boot.rb']) || !path.root? && in_rails_application_subdirectory?(path.parent)
+      end
+    end
+
     def before_loop
-      load_rails
-      super
+      if Ripl::Rails.find_rails_root!
+        load_rails
+        super
+      else
+        abort "Not in a Rails environment" unless File.exists?("#{Dir.pwd}/config/boot.rb")
+      end
     end
 
     def load_rails
-      abort "Not in a Rails environment" unless File.exists?("#{Dir.pwd}/config/boot.rb")
       ENV['RAILS_ENV'] = ARGV[0] if ARGV[0].to_s[/^[^-]/]
 
       require "#{Dir.pwd}/config/boot"
@@ -27,4 +53,4 @@ module Ripl
   end
 end
 
-Ripl::Shell.send :include, Ripl::Rails if defined? Ripl::Shell
+Ripl::Shell.include Ripl::Rails
