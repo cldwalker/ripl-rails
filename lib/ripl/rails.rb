ddb1d565ed4b673b86f84234709cf03e8e478212
Gabriel Horner <gabriel.horner@gmail.com>
2011-03-30 00:07:04 -0400

    reorganize code

--- lib/ripl/rails.rb
+++ lib/ripl/rails.rb
@@ -1,39 +1,37 @@
 require 'ripl'
 require 'pathname'
 
-module Ripl
-  module Rails
-    VERSION = '0.1.1'
+module Ripl::Rails
+  VERSION = '0.1.1'
 
-    def self.find_rails_root!
-      until Pathname.pwd.join('config', 'boot.rb').exist?
-        abort "Not in a Rails environment" if Pathname.pwd.root?
-        Dir.chdir '..'
-      end
+  def self.find_rails_root!
+    until Pathname.pwd.join('config', 'boot.rb').exist?
+      abort "Not in a Rails environment" if Pathname.pwd.root?
+      Dir.chdir '..'
     end
+  end
 
-    def before_loop
-      Ripl::Rails.find_rails_root!
-      load_rails
-      super
+  def self.load_rails
+    ENV['RAILS_ENV'] = ARGV[0] if ARGV[0].to_s[/^[^-]/]
+
+    require "#{Dir.pwd}/config/boot"
+    require 'rails' unless defined? ::Rails
+    if ::Rails.version >= '3.0'
+      Object.const_set :APP_PATH, File.expand_path("#{Dir.pwd}/config/application")
+      require 'rails/console/app'
+      require 'rails/console/helpers'
+      require APP_PATH
+      ::Rails.application.require_environment!
+    else
+      ["#{Dir.pwd}/config/environment", 'console_app', 'console_with_helpers'].each {|e| require e }
     end
+    puts "Loading #{::Rails.env} environment (Rails #{::Rails.version})"
+  end
 
-    def load_rails
-      ENV['RAILS_ENV'] = ARGV[0] if ARGV[0].to_s[/^[^-]/]
-
-      require "#{Dir.pwd}/config/boot"
-      require 'rails' unless defined? ::Rails
-      if ::Rails.version >= '3.0'
-        Object.const_set :APP_PATH, File.expand_path("#{Dir.pwd}/config/application")
-        require 'rails/console/app'
-        require 'rails/console/helpers'
-        require APP_PATH
-        ::Rails.application.require_environment!
-      else
-        ["#{Dir.pwd}/config/environment", 'console_app', 'console_with_helpers'].each {|e| require e }
-      end
-      puts "Loading #{::Rails.env} environment (Rails #{::Rails.version})"
-    end
+  def before_loop
+    Ripl::Rails.find_rails_root!
+    Ripl::Rails.load_rails
+    super
   end
 end
 
